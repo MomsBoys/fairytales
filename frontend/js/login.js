474b5944a52e8626a16c6c4a17c300996f238d3e
VolodyaVechirko <volodya.vechirko@gmail.com>
2016-05-27 00:19:53 +0300

    implemented search bar

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -118,3 +118,31 @@ jQuery.fn.putCursorAtEnd = function() {
     	}
 	});
 };
+
+// search bar sliding
+$(function(){
+  var $searchlink = $('#searchtoggl i');
+  var $searchbar  = $('#searchbar');
+  
+  $('#searchtoggl').on('click', function(e){
+    e.preventDefault();
+    
+    if($(this).attr('id') == 'searchtoggl') {
+      if(!$searchbar.is(":visible")) { 
+        // if invisible we switch the icon to appear collapsable
+        $searchlink.removeClass('fa-search').addClass('fa-search-minus');
+      } else {
+        // if visible we switch the icon to appear as a toggle
+        $searchlink.removeClass('fa-search-minus').addClass('fa-search');
+      }
+      
+      $searchbar.slideToggle(300, function(){
+        // callback after search bar animation
+      });
+    }
+  });
+  
+  $('#searchform').submit(function(e){
+    e.preventDefault(); // stop form submission
+  });
+});
