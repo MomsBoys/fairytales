a3d1849fb3ce91e0af436f69e62454b7614ecd06
burros <957267@gmail.com>
2016-05-30 16:17:35 +0300

    backend v 0.0.1)

---
+++ backend/FairyTales/FairyTales/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FairyTales
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
