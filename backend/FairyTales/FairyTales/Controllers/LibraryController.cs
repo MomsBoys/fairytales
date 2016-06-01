6d2ff147bdba391d6f18710cfb242e7d7d2e5630
Yura <yurii.polyniak@gmail.com>
2016-06-01 23:34:20 +0300

    Created library page

---
+++ backend/FairyTales/FairyTales/Controllers/LibraryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using FairyTales.Entities;
+using FairyTales.Models;
+
+namespace FairyTales.Controllers
+{
+    public class LibraryController : Controller
+    {
+        // GET: Library
+        public ActionResult Index()
+        { 
+            return View(DBManager.GetShortTales(null, null));   
+        }
+    }
+}
---
+++ backend/FairyTales/FairyTales/Models/DBManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using FairyTales.Entities;
+using Type = FairyTales.Entities.Type;
+
+namespace FairyTales.Models
+{
+    public class DBManager
+    { 
+        private static String _rootPath = "http://localhost:1599/Content/Data";
+         
+        public static List<Tale> GetShortTales(List<Category> categories, List<Type> types)
+        {
+            DBFairytaleEntities context = new DBFairytaleEntities();
+            List<Tale> a = context.Tales.Select(v => v).ToList();
+            List<Tale> result = new List<Tale>();
+
+            foreach (var item in a)
+            {
+                item.Cover = $"{_rootPath}/{item.Name}/{item.Cover}";
+                item.Text = $"{_rootPath}/{item.Name}/{item.Text}";
+                if ((categories == null || categories.Any(c => c.Category_ID == item.Category_ID))
+                    && (types == null || types.Any(c => c.Type_ID == item.Type_ID)))
+                {
+                    result.Add(item);        
+                }
+            }
+           // a = a.OrderBy(v => v.Name).Reverse().ToList();
+            return result;
+        }
+        
+        public static List<Category> GetCategories()
+        {
+            DBFairytaleEntities context = new DBFairytaleEntities();
+            return context.Categories.Select(v => v).ToList(); 
+        }
+
+        public static List<Type> GetTypes()
+        {
+            DBFairytaleEntities context = new DBFairytaleEntities();
+            return context.Types.Select(v => v).ToList(); 
+        }
+        
+    }
+}
