0aaefb03ffbbcdbb0eb17f0b30380fac76ac30b1
brosgit <rostislav.burba@gmail.com>
2016-06-06 11:51:13 +0300

    initial commit

---
+++ backend/FairyTales/FairyTales/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using FairyTales.Models;
+
+namespace FairyTales.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Contact/Sender
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Sender(EmailModel emailModel)
+        {
+            EmailSender emailSender = new EmailSender(emailModel);
+            if (emailSender.IsEmailSent())
+            {
+                RedirectToAction("Index", "Contact");
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
+	}
+}
---
+++ backend/FairyTales/FairyTales/Models/EmailSender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+
+namespace FairyTales.Models
+{
+    public class EmailModel
+    {
+        [Required]
+        [Display(Name = "SenderName")]
+        public string SenderName { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "SenderEmail")]
+        public string SenderEmail { get; set; }
+        [Required]
+        [Display(Name = "MessageTitle")]
+        public string MessageTitle { get; set; }
+        [Required]
+        [Display(Name = "MessageText")]
+        public string MessageText { get; set; }
+    }
+    public class EmailSender
+    {
+        private EmailModel _emailModel;
+        public EmailSender(EmailModel emailModel)
+        {
+            _emailModel = emailModel;
+        }
+
+        public bool IsEmailSent()
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                // Адреса почти та пароль правдиві, можна на почту глянути за листом)
+                client.Credentials = new System.Net.NetworkCredential("fairytales.project.tech@gmail.com", "techsupport");
+                MailMessage mm = new MailMessage(_emailModel.SenderEmail, "957267@gmail.com", "Вітання.", "я є)");
+                mm.BodyEncoding = UTF8Encoding.UTF8;
+                mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                client.Send(mm);
+            }
+            catch (Exception)
+            {
+                
+            }
+
+
+            return false;
+        }
+    }
+}
