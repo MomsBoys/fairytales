023b9ab44cbf20f4bb2e413354d4f8174ae56d7a
Andrew <macinsiderorg@gmail.com>
2016-06-13 00:49:34 +0300

    Convert tales to UTF8 format, deleted empty input field from shorttale shared file.

--- backend/FairyTales/FairyTales/Scripts/adminpanel/custom.js
+++ backend/FairyTales/FairyTales/Scripts/adminpanel/custom.js
@@ -1,10 +1,5 @@
-$(function() {
-    $("#content")
-        .on("froalaEditor.initialized", function() {
-            $("#content").parents("form").on("submit", function() {
-                return false;
-            });
-        })
+jQuery(document).ready(function () {
+    jQuery("#content")
         .froalaEditor({
             language: "ua",
             enter: $.FroalaEditor.ENTER_P,
@@ -13,15 +8,10 @@ $(function() {
             toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', '|', 'color', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'indent', 'outdent', 'quote', '|', 'fullscreen', '-', 'undo', 'redo', 'clearFormatting', 'selectAll', '|', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'html']
         });
 
-    $("#article_excerpt")
-        .on("froalaEditor.initialized", function () {
-            $("#article_excerpt").parents("form").on("submit", function () {
-                return false;
-            });
-        })
+    jQuery("#article_excerpt")
         .froalaEditor({
             language: "ua",
-            enter: $.FroalaEditor.ENTER_P,
+            enter: jQuery.FroalaEditor.ENTER_P,
             placeholderText: "Введіть будь ласка короткий опис",
             height: 200,
             toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', '|', 'color', 'paragraphStyle', '|', 'fullscreen']
